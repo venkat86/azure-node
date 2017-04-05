1673b57c6fe71038f2dcda994331690308ea2e07
venkat86 <venkat.odesk86@gmail.com>
2017-04-05 23:00:44 +0530

    Azure sample

---
+++ config/config.js
@@ -0,0 +1,33 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'sample'
+    },
+    port: process.env.PORT || 3000,
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'sample'
+    },
+    port: process.env.PORT || 3000,
+  },
+
+  production: {
+     root: rootPath,
+     app: {
+         name: 'sample'
+     },
+     port: process.env.port,
+   },
+    port: process.env.PORT || 3000,
+  }
+};
+
+module.exports = config[env];
